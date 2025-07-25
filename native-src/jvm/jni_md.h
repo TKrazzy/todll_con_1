ad6bd47f599b3e31e63f513581b48ffa26efefdd
TKrazzy <150093528+TKrazzy@users.noreply.github.com>
2025-07-25 23:49:29 +0200

    Update jni_md.h
    1

--- native-src/jvm/jni_md.h
+++ native-src/jvm/jni_md.h
@@ -1,51 +1,48 @@
-/*
- * Copyright (c) 1996, 2013, Oracle and/or its affiliates. All rights reserved.
- * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- */
-
 #ifndef _JAVASOFT_JNI_MD_H_
 #define _JAVASOFT_JNI_MD_H_
 
-#ifndef __has_attribute
-  #define __has_attribute(x) 0
-#endif
-#if (defined(__GNUC__) && ((__GNUC__ > 4) || (__GNUC__ == 4) && (__GNUC_MINOR__ > 2))) || __has_attribute(visibility)
-  #define JNIEXPORT     __attribute__((visibility("default")))
-  #define JNIIMPORT     __attribute__((visibility("default")))
+// Улучшенная поддержка Windows
+#ifdef _WIN32
+    #ifdef _JNI_IMPLEMENTATION_
+        #define JNIEXPORT __declspec(dllexport)
+    #else
+        #define JNIEXPORT __declspec(dllimport)
+    #endif
+    #define JNIIMPORT __declspec(dllimport)
+    #define JNICALL __stdcall
 #else
-  #define JNIEXPORT
-  #define JNIIMPORT
+    // Поддержка Unix-подобных систем и современных компиляторов
+    #ifndef __has_attribute
+        #define __has_attribute(x) 0
+    #endif
+    #if (defined(__GNUC__) && ((__GNUC__ > 4) || (__GNUC__ == 4) && (__GNUC_MINOR__ > 2))) || __has_attribute(visibility)
+        #define JNIEXPORT     __attribute__((visibility("default")))
+        #define JNIIMPORT     __attribute__((visibility("default")))
+    #else
+        #define JNIEXPORT
+        #define JNIIMPORT
+    #endif
+    #define JNICALL
 #endif
 
-#define JNICALL
-
+// Стандартные типы JNI
 typedef int jint;
-#ifdef _LP64 /* 64-bit Solaris */
-typedef long jlong;
+
+// Улучшенная обработка 64-битных систем
+#ifdef _LP64
+    typedef long jlong;
 #else
-typedef long long jlong;
+    typedef long long jlong;
 #endif
 
 typedef signed char jbyte;
+
+// Добавление недостающих типов для полноценной работы
+typedef unsigned char   jboolean;
+typedef unsigned short  jchar;
+typedef short           jshort;
+typedef float           jfloat;
+typedef double          jdouble;
+typedef jint            jsize;
 
 #endif /* !_JAVASOFT_JNI_MD_H_ */
